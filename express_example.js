f38ee06b423878b307958f05a967a0f04e6c4863
Anton Dahlin <anton.nicolaus@gmail.com>
2019-11-26 16:14:29 +0100

    ejs import

--- express_example.js
+++ express_example.js
@@ -2,23 +2,22 @@ var express = require('express');
 
 
 var app = express();
-//app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 
 const dataRoute = require('./routes/data');
 
 app.use('/data', dataRoute);
 
-app.get('/:opt', function (req, res){ 
-    res.json(
-        {
-            "test":"123", 
-            "struct": {
-                "int":1,
-                "bool":true,
-            },
-        }
-    );
+app.use('/', (res, req, next) => {
+    console.log("recieved request, maybe log?");
+    next();
 });
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard', { title: 'hello there' });
+});
+
+
 
 var server = app.listen(3000, () => {});
 
