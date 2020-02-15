c477193832b3822b60fec591d7e63d53228b72b3
MartinBrathen <marbrt-6@student.ltu.se>
2020-02-15 16:25:32 +0100

    relaxed request limiter

--- express_example.js
+++ express_example.js
@@ -29,7 +29,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minutes
-    max: 200 // limit each IP to 200 requests per windowMs
+    max: 6000 // limit each IP to 200 requests per windowMs
   });
 
 app.use(limiter);
